ccf3ecfd00b8d527569799ffde76772146d89a0b
DmitryMalomoshin <makropulas@gmail.com>
2022-06-25 02:41:02 +0300

    Task6 сделал код красивее

--- DZ1Task6/DZ1Task6.cs
+++ DZ1Task6/DZ1Task6.cs
@@ -1,18 +1,22 @@
-﻿Console.Clear();
-
-Console.WriteLine("Введите целое число, а я проверю, является ли оно чётным :)");
-int number = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine();
-
-if (number%2 == 0)
+﻿class Program
 {
-    Console.Write("Введённое вами число является чётным");
-}
-else
-{
-    Console.Write("Введённое вами число является нечётным");
-}
+    // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+    static void Main(string[] args)
+    {
+        Console.Clear();
 
+        Console.WriteLine("Введите целое число, а я проверю, является ли оно чётным :)");
+        int number = Convert.ToInt32(Console.ReadLine());
 
-// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+        Console.WriteLine();
+
+        if (number % 2 == 0)
+        {
+            Console.Write("Введённое вами число является чётным");
+        }
+        else
+        {
+            Console.Write("Введённое вами число является нечётным");
+        }
+    }
+}

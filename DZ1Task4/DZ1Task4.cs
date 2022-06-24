c2b9f899babbb1bafce69c3617c431b4f0b7775f
DmitryMalomoshin <makropulas@gmail.com>
2022-06-25 02:35:22 +0300

    Task4 сделал код красивее

--- DZ1Task4/DZ1Task4.cs
+++ DZ1Task4/DZ1Task4.cs
@@ -1,24 +1,28 @@
-﻿Console.Clear();
-Console.WriteLine("ВВЕДИТЕ ТРИ ЦЕЛЫХ ЧИСЛА И УЗНАЙТЕ, КАКОЕ ИЗ НИХ ЯВЛЯЕТСЯ МАКСИМАЛЬНЫМ \n");
+﻿class Program
+{
+    // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("ВВЕДИТЕ ТРИ ЦЕЛЫХ ЧИСЛА И УЗНАЙТЕ, КАКОЕ ИЗ НИХ ЯВЛЯЕТСЯ МАКСИМАЛЬНЫМ \n");
 
-Console.Write("Введите первое число ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите первое число ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите третье число ");
-int number3 = Convert.ToInt32(Console.ReadLine());
-int max = number1;
+        Console.Write("Введите третье число ");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+        int max = number1;
 
-Console.WriteLine();
-Console.WriteLine("------------------------");
-Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("------------------------");
+        Console.WriteLine();
 
-if (number2 > number1) max = number2;
-if (number3 > number2) max = number3;
+        if (number2 > number1) max = number2;
+        if (number3 > number2) max = number3;
 
-Console.Write("Из трёх заданных чисел, максимальным является число ");
-Console.WriteLine(max);
-
-// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+        Console.Write("Из трёх заданных чисел, максимальным является число " + max);
+    }
+}
